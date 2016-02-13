fd2047fc22249909b4f8ee8e1db2b46b07ad9cb3
Denis CHARLES <denis.charles@wanadoo.fr>
2016-02-13 14:37:03 +0100

    SQL script generator for CMCA software - version 1.0

---
+++ WpfGenerationSqlCommand/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGenerationSqlCommand
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VueModele _VM;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _VM = new VueModele();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            bool bItemExist = false;
+            if (e.Key == Key.Return)
+            {
+                // TEST DE PRESENCE DE LA TETIERE DANS LA LISTE ACTUELLE
+                foreach(string Row in ListeTetieres.Items)
+                {
+                    if(Row.Trim()==txtTETIERE.Text.Trim())
+                    {
+                        bItemExist = true;
+                    }
+                }
+
+                if (!bItemExist)
+                {
+                    // TEST DE PRESENCE DANS LA BASE DE DONNEES
+                    if (_VM.presenceTetiere(txtTETIERE.Text.Trim()))
+                        LoadTetiere(txtTETIERE.Text.Trim());
+                }
+                txtTETIERE.Text = "";
+            }
+        }
+
+        private void LoadTetiere(string sTetiere)
+        {
+            ListeTetieres.Items.Add(sTetiere);
+        }
+
+        private void btnCLEAR_Click(object sender, RoutedEventArgs e)
+        {
+            ListeTetieres.Items.Clear();
+        }
+
+        private void btnGENERATE_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> sLignes = new List<string>();
+            foreach(string Row in ListeTetieres.Items)
+            {
+                sLignes.Add(Row);
+            }
+            SQL_SCRIPT.Text=_VM.generateSQL(sLignes);
+        }
+    }
+}
---
+++ WpfGenerationSqlCommand/VueModele.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace WpfGenerationSqlCommand
+{
+    class VueModele
+    {
+        /// <summary>
+        /// presenceTetiere = Test de l'existance de la tétière dans la base de données
+        /// </summary>
+        /// <param name="sTetiere">TETIERE A TESTER</param>
+        /// <returns></returns>
+        public bool presenceTetiere(string sTetiere)
+        {
+            bool bRetour = false;
+            DB_CMCADataContext _DB = new DB_CMCADataContext();
+            var query = from c in _DB.CHEVAUX
+                        where c.ID_CHEVAL == sTetiere.Trim()
+                        select c;
+            // EXTRACTION DU NOMBRE D'OCCURRENCES
+            if(query.Count()>0)
+                bRetour = true; // La valeur existe dans la base de données
+            return bRetour;
+        }
+
+        /// <summary>
+        /// generateSQL = Génération du script SQL
+        /// </summary>
+        /// <param name="Lignes">Liste de string contenant les tétières à prendre en compte</param>
+        /// <returns></returns>
+        public string generateSQL(List <string>Lignes)
+        {
+            string prefixeSQL = "";
+            string SQL = "";
+            try
+            {
+                Dictionary<string, string> _ptr = this.lectureFichierXML("config_sql.xml");
+                prefixeSQL = _ptr["PREFIXE"];
+                SQL = "";
+
+                foreach (string Row in Lignes)
+                {
+                    if (SQL == "")
+                        SQL += String.Format(_ptr["FIRST_TOKEN"], Row);
+                    else
+                        SQL += String.Format(_ptr["TOKEN"], Row);
+                }
+                SQL += _ptr["SORTING"];
+                SQL += _ptr["SUFFIXE"];
+                System.Windows.Clipboard.SetText(prefixeSQL + SQL);
+            }
+            catch(Exception Erreur)
+            {
+                MessageBox.Show(Erreur.Message, Erreur.Source, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return prefixeSQL+SQL;
+        }
+
+        public Dictionary <string, string> lectureFichierXML(string sFileName)
+        {
+            Dictionary<string, string> _ptr = new Dictionary<string, string>();  
+            string fullFileName;
+
+            fullFileName = System.IO.Directory.GetCurrentDirectory() +"\\" + sFileName.Trim();
+
+            if(File.Exists(fullFileName)==true)
+            {
+                // LECTURE DU FICHIER XML
+                XmlDocument xml = new XmlDocument();
+                xml.Load(fullFileName);
+                XmlNodeList nodes = xml.SelectNodes("//CONFIG_SQL"); 
+                if(nodes.Count==1)
+                {
+                    foreach(XmlNode Cell in nodes[0].ChildNodes)
+                    {
+                        switch(Cell.Name)
+                        {
+                            case "PREFIXE": _ptr.Add("PREFIXE", Cell.InnerText); break;
+                            case "FIRST_TOKEN": _ptr.Add("FIRST_TOKEN", Cell.InnerText); break;
+                            case "TOKEN": _ptr.Add("TOKEN", Cell.InnerText); break;
+                            case "SUFFIXE": _ptr.Add("SUFFIXE", Cell.InnerText); break;
+                            case "SORTING": _ptr.Add("SORTING", Cell.InnerText); break;
+                        }
+                    }
+                }
+            }
+            return _ptr;
+        }
+    }
+}
